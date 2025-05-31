476d7db27740fbb3cf8b0fafad71fa989c6a62f8
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2025-05-31 21:14:08 +0300

    Streams, Files and Directories

--- src/L04_Streams_Files_And_Directories/P08_GetFolderSize.java
+++ src/L04_Streams_Files_And_Directories/P08_GetFolderSize.java
@@ -1,10 +1,27 @@
 package L04_Streams_Files_And_Directories;
 
-import java.util.Scanner;
+import java.io.File;
 
 public class P08_GetFolderSize {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        String path = "src/L04_Streams_Files_And_Directories/resourses/Exercises Resources";
+
+        File folder = new File(path);
+
+        File[] allFiles = folder.listFiles();
+
+        int folderSize = 0;
+
+        for (File file : allFiles){
+            int currentSize = (int) file.length();
+            folderSize += currentSize;
+        }
+
+        System.out.println("Folder size: " + folderSize);
+
+
+
 
 
     }
