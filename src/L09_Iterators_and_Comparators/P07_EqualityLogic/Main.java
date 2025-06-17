ebd092559a080e718b827922b7cfdd1b79e7b51f
YuliyanKastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2025-06-17 23:48:01 +0300

    Iterators and Comparator - Exercises 7, 8, 9

---
+++ src/L09_Iterators_and_Comparators/P07_EqualityLogic/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<Person> treeSet = new TreeSet<>();
+        Set<Person> hashSet = new HashSet<>();
+
+
+        while (n-- > 0) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String name = tokens[0];
+            int age = Integer.parseInt(tokens[1]);
+
+            Person person = new Person(name, age);
+            treeSet.add(person);
+            hashSet.add(person);
+
+        }
+
+        System.out.println(treeSet.size());
+        System.out.println(hashSet.size());
+    }
+}
